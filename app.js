ca458a40851594a26a5bb5a30173578eb274c74d
Khay <altaircarn@gmail.com>
2019-10-24 11:01:13 -0500

    subiendo el proyecto a github

---
+++ app.js
@@ -0,0 +1,42 @@
+const flightPath = {
+    curviness: 1.25,
+    autoRotate: true,
+    values: [
+        {x: 100, y: -20},
+        {x:300, y:10},
+        {x:500, y:100},
+        {x:750, y:-100},
+        {x:350, y:-50},
+        {x:600, y:100},
+        {x:800, y:0},
+        {x: 1000, y:-250},
+        {x:1200, y: 100},
+        {x:1300, y: -100},
+        {x:1000, y:-50},
+        {x:900, y: 100},
+        {x:1100, y: 50},
+        {x:1400, y: 300},
+        {x:1600, y :100}
+    ]
+}
+
+const tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to('.paper-plane', 1,{
+        bezier: flightPath,
+        ease: Power1.easeInOut
+    })
+);
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 2000,
+    triggerHook: 0
+
+})
+.setTween(tween)
+.setPin('.animation')
+.addTo(controller);
